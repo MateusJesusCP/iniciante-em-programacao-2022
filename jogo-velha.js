c9fad2192eebee245d328538e9402f0b9434fe6f
Mateus de Jesus <87149396+MateusJesusCP@users.noreply.github.com>
2022-07-14 18:49:56 -0300

    jogo_da_velha
    Infelizmente fui incapaz de concluir como o esperado

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,10 +1,19 @@
+// http://127.0.0.1:5500/iniciante-em-programacao-2022/
 const X = "X";
 const O = "O";
 
+
+let player = marcarJogadorAtivo;
+
 function reiniciarJogo() {
-    
+    document. location. reload()
 }
 
 function selecionarArea(posicaoLinha, posicaoColuna) {
+console.log("Chamando a função");
+console.log(posicaoLinha, posicaoColuna)
 
+ desenharSimbolo(X, posicaoLinha,posicaoColuna)
+ desenharSimbolo(O, posicaoLinha,posicaoColuna)
 }
+
